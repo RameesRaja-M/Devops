4a68c359605c04b91f82803185eb03fba8a197b4
RameesRaja-M <rameesraja27082002@gmail.com>
2026-01-07 16:42:43 +0530

    add all eslint,prettier and ci changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -60,7 +60,7 @@ function App() {
     fetchTasks();
   };
 
-  // let sangam = 'unused variable here';
+  // let ramees = 'unused variable here';
 
   return (
     <div className="min-h-screen bg-gray-50">
